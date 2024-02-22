04b03559c083c5cb24d78d3c2ae0e1eb77496dcd
joel <bonjourjoel@gmail.com>
2024-02-22 13:47:10 +0100

    tailwind responsive tutorial

--- src/app/responsive/page.tsx
+++ src/app/responsive/page.tsx
@@ -1,3 +1,27 @@
+import Image from "next/image";
+
 export default function ResponsivePage() {
-  return <div>ResponsivePage</div>;
+  return (
+    <div className="m-4 text-center">
+      <section className="grid gap-8 md:grid-cols-2 md:items-center md:text-left">
+        <div className="relative h-80 w-full">
+          <Image
+            className="rounded-lg object-cover"
+            src="/disneyland-food-festival.jpg"
+            alt="mickey"
+            fill={true}
+          />
+        </div>
+        <div className="tablet:bg-red-300">
+          <h1 className="mb-2 text-4xl font-medium">Headline</h1>
+          <p className="md:max-lg:bg-blue-50">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum cum
+            esse aperiam veniam nostrum vero molestias, eveniet perspiciatis
+            quidem reprehenderit distinctio nihil architecto temporibus adipisci
+            voluptas eos doloribus deleniti laborum!
+          </p>
+        </div>
+      </section>
+    </div>
+  );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,9 +7,17 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    /* items defined here erase completely the default tailwind config and replace it */
+    /* ... */
+
     extend: {
+      /* items in extend are added or updated from base tailwind config */
       colors: {
         "twitter-blue": "#1DA1F2",
+      },
+      screens: {
+        tablet: "690px" /* add a breakpoint type */,
+        /* can also change "md" for example */
       },
     },
   },
